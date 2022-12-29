b54ac38df8364a72cff25fe90c410e44ee12d9f5
Vincent <vincent.vonrotz@gmail.com>
2022-12-29 18:52:46 +0100

    Some refactoring on day 24 to make it more legible

--- Day 24/src/main.rs
+++ Day 24/src/main.rs
@@ -2,8 +2,9 @@ use std::{fs, path::Path, collections::HashSet};
 extern crate queues;
 use queues::*;
 
-// 'O' = all directions; 'U' = All directions but up, 'n' = all directions but down, 'C' = all directions but right, 'D' = all directions but left
+const CANDIDATES: [(i8, i8); 5] = [(0,1), (1,0), (-1,0), (0,-1), (0,0)];
 
+// Looks complicated, but just checks if there will be a blizzard at i,j at time 'time' (prevents us having to actually update the grid)
 fn storm_at_time(grid: &Vec<Vec<char>>, i: usize, j: usize, time: usize) -> bool {
     if j == 0 || j == grid[i].len() - 1 {return true;}
     if (i==0 && j != 1) || (i == grid.len() - 1 && j != grid[i].len() - 2) {return true;}
@@ -36,27 +37,16 @@ fn storm_at_time(grid: &Vec<Vec<char>>, i: usize, j: usize, time: usize) -> bool
     false
 }
 
-const CANDIDATES: [(i8, i8); 5] = [(0,1), (1,0), (-1,0), (0,-1), (0,0)];
-
-fn main() {
-    let data = fs::read_to_string(Path::new("src/input.txt"))
-        .expect("Should have been able to read the file");
-    
-    let grid = data.split("\n").map(|s| s.split("").filter(|q| q.len() > 0).map(|p| p.chars().nth(0).unwrap()).collect::<Vec<_>>()).collect::<Vec<_>>();
-
-    let mut bfs = queue![((0, 1), 0)];
-
-    let part1;
-
+fn time_to(grid: &Vec<Vec<char>>, start_i: usize, start_j: usize, start_time: usize, end_i: usize, end_j: usize) -> usize {
+    let mut bfs = queue![((start_i, start_j), start_time)];
     let mut visited = HashSet::new();
-
+    
     loop {
         let (pos, time) = bfs.remove().expect("Failure");
-        if pos == (grid.len() - 1, grid[0].len() - 2) {
-            part1 = time;
-            break;
+        if pos == (end_i, end_j) {
+            return time;
         }
-        CANDIDATES.iter().filter(|(x,_)| pos.0 > 0 || x >= &0)
+        CANDIDATES.iter().filter(|(x,_)| (pos.0 > 0 || x >= &0) && (((pos.0 as i8 + *x) as usize) < grid.len()))
             .filter(|(x, y)| !storm_at_time(&grid, (pos.0 as i8 + x) as usize, (pos.1 as i8 + y) as usize, time + 1))
             .for_each(|(x, y)| {
                 let candidate = (((pos.0 as i8 + x) as usize, (pos.1 as i8 + y) as usize), time + 1);
@@ -64,42 +54,22 @@ fn main() {
                     bfs.add(candidate).expect("Couldn't add to queue");
                 }
             });
-    }
-    let mut bfs_2 = queue![((grid.len() - 1, grid[0].len() - 2), part1)];
-    let mut part2;
-    visited.clear();
-    loop {
-        let (pos, time) = bfs_2.remove().expect("Failure");
-        if pos == (0, 1) {
-            part2 = time;
-            break;
         }
-        CANDIDATES.iter().filter(|(x,_)| ((pos.0 as i8 + *x) as usize) < grid.len())
-            .filter(|(x, y)| !storm_at_time(&grid, (pos.0 as i8 + x) as usize, (pos.1 as i8 + y) as usize, time + 1))
-            .for_each(|(x, y)| {
-                let candidate = (((pos.0 as i8 + x) as usize, (pos.1 as i8 + y) as usize), time + 1);
-                if visited.insert(candidate) {
-                    bfs_2.add(candidate).expect("Couldn't add to queue");
-                }
-            });
     }
-    let mut bfs_3 = queue![((0, 1), part2)];
-    visited.clear();
-    loop {
-        let (pos, time) = bfs_3.remove().expect("Failure");
-        if pos == (grid.len() - 1, grid[0].len() - 2) {
-            part2 = time;
-            break;
-        }
-        CANDIDATES.iter().filter(|(x,_)| pos.0 > 0 || x >= &0)
-            .filter(|(x, y)| !storm_at_time(&grid, (pos.0 as i8 + x) as usize, (pos.1 as i8 + y) as usize, time + 1))
-            .for_each(|(x, y)| {
-                let candidate = (((pos.0 as i8 + x) as usize, (pos.1 as i8 + y) as usize), time + 1);
-                if visited.insert(candidate) {
-                    bfs_3.add(candidate).expect("Couldn't add to queue");
-                }
-            });
-    }
+    
+fn main() {
+    let data = fs::read_to_string(Path::new("src/input.txt"))
+        .expect("Should have been able to read the file");
+    
+    let grid = data.split("\n").map(|s| s.split("").filter(|q| q.len() > 0).map(|p| p.chars().nth(0).unwrap()).collect::<Vec<_>>()).collect::<Vec<_>>();
+    let (end_i, end_j) = (grid.len() - 1, grid[0].len() - 2);
+
+    //Part 1: 262,
+    let part1 = time_to(&grid, 0, 1, 0, end_i, end_j);
+    
+    let back = time_to(&grid, end_i, end_j, part1, 0, 1);
+    //Part 2: 785
+    let part2 = time_to(&grid, 0, 1, back, end_i, end_j);
 
     println!("Part 1: {},\nPart 2: {}", part1, part2);
 }
